525fd894c0c1155b44a6979f20143203a95b9eab
Scott Garton <garton.scott@gmail.com>
2021-03-27 18:17:06 +1100

    Adds login page and shows when not authorised

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,10 +1,18 @@
 import * as React from "react";
-import './App.css';
+import { useState } from "react";
+import './App.scss';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Dashboard from '../Dashboard/Dashboard';
+import Login from '../Login/Login';
 import Preferences from '../Preferences/Preferences';
 
 function App() {
+  const [token, setToken] = useState();
+
+  if (!token) {
+    return <Login setToken={setToken} />
+  }
+
   return (
     <div className="wrapper">
       <h1>Application</h1>
---
+++ src/components/Login/Login.tsx
@@ -0,0 +1,29 @@
+import * as React from 'react';
+import './Login.scss';
+
+interface LoginProps {
+  setToken: React.Dispatch<React.SetStateAction<undefined>>
+}
+
+const Login: React.FunctionComponent<LoginProps> = () => {
+  return (
+    <div className="login-wrapper">
+      <h1>Please Log In</h1>
+      <form>
+        <label>
+          <p>Username</p>
+          <input type="text" />
+        </label>
+        <label>
+          <p>Password</p>
+          <input type="password" />
+        </label>
+        <div>
+          <button type="submit">Submit</button>
+        </div>
+      </form>
+    </div>
+  )
+}
+
+export default Login;
