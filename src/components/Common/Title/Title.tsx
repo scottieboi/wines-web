1b85afa8bd2a7d02f77e156984c05075c93cc912
Scott Garton <garton.scott@gmail.com>
2021-04-22 17:02:06 +1000

    Build start of Add wine form

--- src/components/Common/Title/Title.tsx
+++ src/components/Common/Title/Title.tsx
@@ -3,15 +3,31 @@ import Typography from "@material-ui/core/Typography";
 
 interface TitleProps {
   children: React.ReactNode;
+  subtitle?: boolean;
+  className?: string;
 }
 
-const Title: React.FunctionComponent<TitleProps> = (props: TitleProps) => {
-  const { children } = props;
+const Title: React.FunctionComponent<TitleProps> = ({
+  children,
+  subtitle,
+  className,
+}: TitleProps) => {
   return (
-    <Typography component="h2" variant="h6" color="primary" gutterBottom>
+    <Typography
+      className={className}
+      component="h2"
+      variant={subtitle ? "subtitle1" : "h6"}
+      color="primary"
+      gutterBottom
+    >
       {children}
     </Typography>
   );
+};
+
+Title.defaultProps = {
+  subtitle: false,
+  className: undefined,
 };
 
 export default Title;
--- src/components/Pages/AddWine/AddWine.tsx
+++ src/components/Pages/AddWine/AddWine.tsx
@@ -1,13 +1,102 @@
 import * as React from "react";
+import { makeStyles, TextField } from "@material-ui/core";
 import { Page } from "../../Common/Page";
 import { Loading } from "../../Common/Loading";
 import { Tile } from "../../Common/Tile";
+import { Title } from "../../Common/Title";
+
+const useStyles = makeStyles((theme) => {
+  const textFieldMargin = theme.spacing(1);
+  return {
+    form: {
+      maxWidth: "800px",
+    },
+    formGroup: {
+      marginBottom: theme.spacing(1),
+    },
+    formGroupTitle: {
+      marginTop: theme.spacing(4),
+    },
+    textField: {
+      marginLeft: textFieldMargin,
+      marginRight: textFieldMargin,
+      width: "25ch",
+    },
+    notesField: {
+      marginLeft: textFieldMargin,
+      marginRight: textFieldMargin,
+      width: `calc(100% - ${textFieldMargin * 2}px)`,
+    },
+  };
+});
 
 const AddWine = (): JSX.Element => {
+  const classes = useStyles();
+  const yearInputProps = {
+    min: 1900,
+    max: 3000,
+    step: 1,
+  };
+  const currencyInputProps = {
+    min: 0,
+    step: 0.01,
+  };
+
   return (
     <Page titleText="Add a wine">
       <Tile>
-        <Loading />
+        <Title>New wine</Title>
+        <form className={classes.form} noValidate autoComplete="off">
+          <div className={classes.formGroup}>
+            <TextField label="Wine name" className={classes.textField} />
+            <TextField label="Vineyard" className={classes.textField} />
+            <TextField label="Wine type" className={classes.textField} />
+            <TextField label="Region" className={classes.textField} />
+            <TextField
+              label="Vintage"
+              className={classes.textField}
+              type="number"
+              inputProps={yearInputProps}
+            />
+          </div>
+          <div className={classes.formGroup}>
+            <Title subtitle className={classes.formGroupTitle}>
+              More details
+            </Title>
+            <TextField
+              label="Year bought"
+              className={classes.textField}
+              type="number"
+              inputProps={yearInputProps}
+            />
+            <TextField
+              label="Drink from (year)"
+              className={classes.textField}
+              type="number"
+              inputProps={yearInputProps}
+            />
+            <TextField
+              label="Drink to (year)"
+              className={classes.textField}
+              type="number"
+              inputProps={yearInputProps}
+            />
+            <TextField
+              label="Price paid"
+              className={classes.textField}
+              type="number"
+              inputProps={currencyInputProps}
+            />
+            <TextField label="Rating" className={classes.textField} />
+            <TextField label="Bottle size" className={classes.textField} />
+            <TextField label="Notes" multiline className={classes.notesField} />
+          </div>
+          <div>
+            <Title subtitle className={classes.formGroupTitle}>
+              Location in cellar
+            </Title>
+          </div>
+        </form>
       </Tile>
     </Page>
   );
