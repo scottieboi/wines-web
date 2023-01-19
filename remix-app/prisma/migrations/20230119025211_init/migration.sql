6743e12553c06d4675b9dd6e09ecd647fdd3861e
Scott Garton <N/A>
2023-01-19 13:53:11 +1100

    redo initial migrate

--- remix-app/prisma/migrations/20230119025211_init/migration.sql
+++ remix-app/prisma/migrations/20230119025211_init/migration.sql
@@ -1,8 +1,10 @@
 -- CreateTable
 CREATE TABLE "box" (
     "id" TEXT NOT NULL,
-    "boxno" INTEGER NOT NULL,
+    "identifier" TEXT NOT NULL,
     "size" TEXT,
+    "description" TEXT,
+    "userid" TEXT NOT NULL,
 
     CONSTRAINT "PK_box" PRIMARY KEY ("id")
 );
@@ -10,10 +12,10 @@ CREATE TABLE "box" (
 -- CreateTable
 CREATE TABLE "location" (
     "id" TEXT NOT NULL,
-    "wineid" TEXT,
-    "no" INTEGER,
-    "boxid" TEXT,
+    "number" INTEGER,
     "cellarversion" SMALLINT,
+    "boxid" TEXT NOT NULL,
+    "wineid" TEXT NOT NULL,
 
     CONSTRAINT "PK_location" PRIMARY KEY ("id")
 );
@@ -22,6 +24,7 @@ CREATE TABLE "location" (
 CREATE TABLE "region" (
     "id" TEXT NOT NULL,
     "region" TEXT NOT NULL,
+    "userid" TEXT,
 
     CONSTRAINT "PK_region" PRIMARY KEY ("id")
 );
@@ -30,6 +33,7 @@ CREATE TABLE "region" (
 CREATE TABLE "vineyard" (
     "id" TEXT NOT NULL,
     "vineyard" TEXT NOT NULL,
+    "userid" TEXT,
 
     CONSTRAINT "PK_vineyard" PRIMARY KEY ("id")
 );
@@ -39,7 +43,6 @@ CREATE TABLE "winelist" (
     "id" TEXT NOT NULL,
     "vintage" SMALLINT,
     "winename" TEXT,
-    "winetypeid" TEXT,
     "percentalcohol" REAL,
     "pricepaid" DECIMAL(7,2),
     "yearbought" SMALLINT,
@@ -50,6 +53,7 @@ CREATE TABLE "winelist" (
     "drinkrangeto" SMALLINT,
     "regionid" TEXT,
     "vineyardid" TEXT,
+    "winetypeid" TEXT,
     "userid" TEXT NOT NULL,
 
     CONSTRAINT "PK_winelist" PRIMARY KEY ("id")
@@ -59,6 +63,7 @@ CREATE TABLE "winelist" (
 CREATE TABLE "winetype" (
     "id" TEXT NOT NULL,
     "winetype" TEXT NOT NULL,
+    "userid" TEXT,
 
     CONSTRAINT "PK_winetype" PRIMARY KEY ("id")
 );
@@ -75,6 +80,9 @@ CREATE TABLE "user" (
 );
 
 -- CreateIndex
+CREATE INDEX "box_userid_fk" ON "box"("userid");
+
+-- CreateIndex
 CREATE INDEX "location_boxid_fk" ON "location"("boxid");
 
 -- CreateIndex
@@ -84,7 +92,13 @@ CREATE INDEX "location_wineid_fk" ON "location"("wineid");
 CREATE UNIQUE INDEX "region_region_key" ON "region"("region");
 
 -- CreateIndex
+CREATE INDEX "region_userid_fk" ON "region"("userid");
+
+-- CreateIndex
 CREATE UNIQUE INDEX "vineyard_vineyard_key" ON "vineyard"("vineyard");
+
+-- CreateIndex
+CREATE INDEX "vineyard_userid_fk" ON "vineyard"("userid");
 
 -- CreateIndex
 CREATE INDEX "winelist_regionid_fk" ON "winelist"("regionid");
@@ -102,13 +116,25 @@ CREATE INDEX "winelist_userid_fk" ON "winelist"("userid");
 CREATE UNIQUE INDEX "winetype_winetype_key" ON "winetype"("winetype");
 
 -- CreateIndex
+CREATE INDEX "winetype_userid_fk" ON "winetype"("userid");
+
+-- CreateIndex
 CREATE UNIQUE INDEX "user_username_key" ON "user"("username");
+
+-- AddForeignKey
+ALTER TABLE "box" ADD CONSTRAINT "box_userid_fkey" FOREIGN KEY ("userid") REFERENCES "user"("id") ON DELETE RESTRICT ON UPDATE NO ACTION;
 
 -- AddForeignKey
 ALTER TABLE "location" ADD CONSTRAINT "location_boxid_fkey" FOREIGN KEY ("boxid") REFERENCES "box"("id") ON DELETE RESTRICT ON UPDATE NO ACTION;
 
 -- AddForeignKey
 ALTER TABLE "location" ADD CONSTRAINT "location_wineid_fkey" FOREIGN KEY ("wineid") REFERENCES "winelist"("id") ON DELETE RESTRICT ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "region" ADD CONSTRAINT "region_userid_fkey" FOREIGN KEY ("userid") REFERENCES "user"("id") ON DELETE RESTRICT ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "vineyard" ADD CONSTRAINT "vineyard_userid_fkey" FOREIGN KEY ("userid") REFERENCES "user"("id") ON DELETE RESTRICT ON UPDATE NO ACTION;
 
 -- AddForeignKey
 ALTER TABLE "winelist" ADD CONSTRAINT "winelist_regionid_fkey" FOREIGN KEY ("regionid") REFERENCES "region"("id") ON DELETE RESTRICT ON UPDATE NO ACTION;
@@ -121,3 +147,6 @@ ALTER TABLE "winelist" ADD CONSTRAINT "winelist_winetypeid_fkey" FOREIGN KEY ("w
 
 -- AddForeignKey
 ALTER TABLE "winelist" ADD CONSTRAINT "winelist_userid_fkey" FOREIGN KEY ("userid") REFERENCES "user"("id") ON DELETE RESTRICT ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "winetype" ADD CONSTRAINT "winetype_userid_fkey" FOREIGN KEY ("userid") REFERENCES "user"("id") ON DELETE RESTRICT ON UPDATE NO ACTION;
