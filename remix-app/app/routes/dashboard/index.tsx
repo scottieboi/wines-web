b9e6524af29ab375811199b827ad4d28901f5a0a
Scott Garton <N/A>
2023-02-03 23:35:19 +1100

    add dummy table to dashboard

--- remix-app/app/routes/dashboard/index.tsx
+++ remix-app/app/routes/dashboard/index.tsx
@@ -1,9 +1,11 @@
 import { Link } from "@remix-run/react";
+import DashboardTable from "~/components/DashboardTable";
 
 export default function DashboardRoute() {
   return (
     <div className="container mx-auto">
       <h2 className="my-4 text-2xl">Dashboard</h2>
+      <DashboardTable />
       <Link className="btn-primary btn" to="add-wine">
         + Add wine
       </Link>
